c4e88bfe9dd6da0c09227d14a74849cf16a2db02
Seunghwan-Seo1 <oosssok1@gmail.com>
2024-09-10 18:27:15 +0900

    day1(Review/Lodge)

---
+++ portfolio/src/main/java/com/mysite/portfolio/review/ReviewService.java
@@ -0,0 +1,30 @@
+package com.mysite.portfolio.review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+	
+	@Autowired
+	private ReviewRepository reviewRepository;
+	
+	
+	//리뷰 작성
+	
+	
+	
+	
+	//리뷰 리스트
+	
+	
+	
+	//리뷰 수정
+	
+	
+	
+	//리뷰 삭제
+	
+	
+}
+
