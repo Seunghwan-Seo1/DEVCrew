f80d286a8b28801addc2252bde8858233aaf9b62
Seunghwan-Seo1 <oosssok1@gmail.com>
2024-09-10 15:50:32 +0900

    day1(main page/layout)

---
+++ portfolio/src/main/java/com/mysite/portfolio/admin/AdminController.java
@@ -0,0 +1,19 @@
+package com.mysite.portfolio.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+@Controller
+public class AdminController {
+	
+	@GetMapping("/main")
+	public String adminmain() {
+		return "admin/main";
+	}
+	
+}
