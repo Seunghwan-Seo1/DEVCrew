fa52c976a366373242f7c06d2e9797bb35f65737
hyunsik <wjs478wjs@naver.com>
2024-09-11 15:28:52 +0900

    day2
    day2

--- portfolio/src/main/java/com/mysite/portfolio/festival/FestivalController.java
+++ portfolio/src/main/java/com/mysite/portfolio/festival/FestivalController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ public class FestivalController {
 
 	// create
 	@GetMapping("/create")
-	public String create() {
+	public String create() {        //축제 등록
 		return "festival/create";
 	}
 
@@ -34,13 +35,13 @@ public class FestivalController {
 	}
 	
 	// readlist
-	@GetMapping("/readlist")
+	@GetMapping("/readlist")                      //축제목록
 	public String readlist(Model model) {
 		model.addAttribute("festivals", festivalService.readlist()); 
 		return "festival/readlist";
 	}
 	
-	@GetMapping("/readdetail")
+	@GetMapping("/readdetail")              //축제내용 상세보기
 	public String readdetail() {
 		
 		return "festival/readdetail";
@@ -55,24 +56,25 @@ public class FestivalController {
 		model.addAttribute("downpath", "https://" + downpath);
 		
 		return "festival/readdetail";*/
-	}
+	
 	//update
-//		@GetMapping("/update/{id}")
-//		public String update(Model model, @PathVariable("id") Integer id) {
-//			model.addAttribute("product", festivalService.readdetail(id));
-//			return "festival/update";
-//		}
-//		
-//		@PostMapping("/update")
-//		public String update(@ModelAttribute Festival festival, MultipartFile file) throws IOException {
-//			festivalService.update(festival, file);
-//			return "product/readdetail/" + festival.getFid();
-//		}
-//		//delete
-//		@GetMapping("/delete/{id}")
-//		public String delete(@PathVariable("id") Integer id) {
-//			
-//			return "redirect:/festival/readlist";
-//		}
-//		
+		@GetMapping("/update/{id}")
+		public String update(Model model, @PathVariable("id") Integer id) {
+			model.addAttribute("product", festivalService.readdetail(id));
+			return "festival/update";
+		}
+		
+		@PostMapping("/update")
+		public String update(@ModelAttribute Festival festival, MultipartFile file) throws IOException {
+			festivalService.update(festival, file);
+			return "festival/readdetail/" + festival.getFid();
+		}
+		//delete
+		@GetMapping("/delete/{id}")
+		public String delete(@PathVariable("id") Integer id) {
+			
+			return "redirect:/festival/readlist";
+		}
+}
+		
 
--- portfolio/src/main/java/com/mysite/portfolio/festival/FestivalService.java
+++ portfolio/src/main/java/com/mysite/portfolio/festival/FestivalService.java
@@ -1,58 +1,62 @@
-//전현식
 package com.mysite.portfolio.festival;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mysite.portfolio.S3Service;
+
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Service
 public class FestivalService {
-	
-	@Autowired
-	   private FestivalRepository festivalRepository;
+    
+    @Autowired
+    private FestivalRepository festivalRepository;
 
-	   //readlist
-	   List<Festival> readlist() {
-	      return festivalRepository.findAll();
-	   }
-	   
-	   //readdetail
-	   Festival readdetail(Integer id) {
-	      Optional<Festival> ob = festivalRepository.findById(id);
-	      return ob.get();
-	   }
-	   
-	   //create
-	   public void create(String fname, String flocation, String fdescription) {
-	        Festival festival = new Festival();
-	        festival.setFname(fname);
-	        festival.setFlocation(flocation);
-	        festival.setFdescription(fdescription);
-	        festival.setFdate(LocalDateTime.now());
-	        
-	        
-	        this.festivalRepository.save(festival);
+    @Autowired
+    private S3Service s3Service; // S3Service 주입
 
-}
+    // create
+    public void create(Festival festival, MultipartFile file) {
+        festival.setFdate(LocalDateTime.now()); // 데이터를 등록
+        this.festivalRepository.save(festival);
+    }
 
-	public void create1(Festival festival, MultipartFile file) {
-		// TODO Auto-generated method stub
-		
-	}
+    // readlist
+    public List<Festival> readlist() {
+        return festivalRepository.findAll();
+    }
 
-	public void create(Festival festival, MultipartFile file) {
-		// TODO Auto-generated method stub
-		
-	}
+    // readdetail
+    public Festival readdetail(Integer fid) {
+        return festivalRepository.findById(fid)
+            .orElseThrow(() -> new RuntimeException("Festival not found"));
+    }
 
-	public void update(Festival festival, MultipartFile file) {
-		// TODO Auto-generated method stub
-		
-	}
+    // update
+    public void update(Festival festival, MultipartFile file) throws IOException {
+        if (file != null && !file.isEmpty()) {
+            // 새 사진을 넣을 경우
+            UUID uuid = UUID.randomUUID();
+            String fileName = uuid + "_" + file.getOriginalFilename();
+            s3Service.uploadFile(file, fileName);
+            festival.setFimg(fileName); // 새로운 uuid 붙인 사진 넣기
+        } else {
+            // 기존 사진 이름을 그대로 사용
+            festival.setFimg(festival.getFimg());
+        }
+        festivalRepository.save(festival);
+    }
+
+    // delete
+    public void delete(Integer id) {
+        festivalRepository.deleteById(id);
+    }
 }
