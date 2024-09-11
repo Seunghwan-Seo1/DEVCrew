d4fd79fca23f7b9b07afe9e0ce4658da4cae18b5
LeeJinHo <yjg03208@naver.com>
2024-09-11 11:33:47 +0900

    day2(member)

--- portfolio/src/main/java/com/mysite/portfolio/festival/FestivalController.java
+++ portfolio/src/main/java/com/mysite/portfolio/festival/FestivalController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+@RequestMapping("/festival")
 @Controller
 public class FestivalController {
 	
