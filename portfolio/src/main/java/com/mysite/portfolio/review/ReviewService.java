57d3eb97bfb8da2b340cd2c952988a9683ff5624
LeeeYumin <152114081+LeeeYumin@users.noreply.github.com>
2024-09-11 09:52:48 +0900

    머지테스트
    개인브랜치 테스트

--- portfolio/src/main/java/com/mysite/portfolio/review/ReviewService.java
+++ portfolio/src/main/java/com/mysite/portfolio/review/ReviewService.java
@@ -1,5 +1,7 @@
 package com.mysite.portfolio.review;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,20 +13,29 @@ public class ReviewService {
 	
 	
 	//리뷰 작성
-	
-	
+	public void create(Review review) {
+		review.setUsername(null);
+		
+		reviewRepository.save(review);
+	}
 	
 	
 	//리뷰 리스트
-	
+	List<Review> rvlist() {
+		return reviewRepository.findAll(); // 전체 목록 조회
+	}
 	
 	
 	//리뷰 수정
-	
+	public void rupdate(Review review) {
+		reviewRepository.save(review);
+	}
 	
 	
 	//리뷰 삭제
-	
+	public void rdelete (Integer rnum) {
+		reviewRepository.deleteById(rnum);
+	}
 	
 }
 
