058860e6d23ff92509383989ecbaf63ae3562a1f
LeeJinHo <yjg03208@naver.com>
2024-09-11 09:51:25 +0900

    day2(user)

--- hyunsik/portfolio/src/main/java/com/mysite/portfolio/review/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.portfolio.review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-
-}
