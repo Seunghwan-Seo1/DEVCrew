30d3f969a99f476b24e0eeaa01844ba68ba3a523
JEONHYUNSIK <wjs478wjs@naver.com>
2024-09-26 16:07:13 +0900

    day13
    day13

--- portfolio/src/main/java/com/mysite/portfolio/festival/FestivalRepository.java
+++ portfolio/src/main/java/com/mysite/portfolio/festival/FestivalRepository.java
@@ -7,10 +7,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FestivalRepository extends JpaRepository<Festival, Integer> {
 	
 	@Query("SELECT f FROM Festival f ORDER BY size(f.voter) DESC")
     List<Festival> findAllByOrderByVoteCountDesc(); // 추천 수 기준 내림차순 정렬
+	
+	//검색 기능       
+	@Query("SELECT f FROM Festival f WHERE f.fname like %:keyword%")
+	List<Festival> findAllByfnameLike(@Param("keyword") String keyword);
    
 }
