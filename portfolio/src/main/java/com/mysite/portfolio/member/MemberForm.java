1ec44bf070109aabaa80ba17bfe609be63f3460d
Seunghwan-Seo1 <oosssok1@gmail.com>
2024-09-30 17:35:59 +0900

    day18

--- portfolio/src/main/java/com/mysite/portfolio/member/MemberForm.java
+++ portfolio/src/main/java/com/mysite/portfolio/member/MemberForm.java
@@ -10,23 +10,22 @@ import lombok.Data;
 @Data
 public class MemberForm {
 
-	@NotEmpty(message = "ID를 이메일 형식으로 입력해주세요")
-	@Email
-	private String username;
-	
-	@Pattern (regexp = "^[a-zA-Z0-9!@#$%^&*]{4,16}$",
-			  message = "비밀번호는 4~16자의 영문 대소문자,숫자,특수문자로 구성되어야 합니다")
-	@NotEmpty(message = "비밀번호를 입력해주세요")
+   @NotEmpty(message = "ID를 이메일 형식으로 입력해주세요")
+   @Email
+   private String username;
+   
+   @Pattern(regexp  = "^[a-zA-Z0-9!@#$%^&*]{4,16}$",
+          message = "비밀번호는 4~16자의 영문 대소문자,숫자,특수문자로 구성되어야 합니다")
     private String password1;
 
     @NotEmpty(message = "비밀번호를 다시 입력해주세요")
     private String password2;
-	
+   
     @Size(min = 4, max = 10, message = "닉네임은 4~10자로 입력해주세요")
-    @NotEmpty(message = "닉네임을 입력해주세요")
     private String nickname;
     
-    @NotEmpty(message = "연락처를 입력해주세요")
-	private String maddr;
-	
+    @Pattern(regexp  = "^(010\\d{8}|(02|0(3[1-3]|4[1-4]|5[1-5]|6[1-4]))\\d{7,8})$",
+           message = "-를 제외한 전화번호를 입력해주세요")
+   private String maddr;
+   
 }
